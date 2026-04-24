0330611d45985c990c9dcd89dad0790bce385d99
AgroSmart Developer <dev@agrosmart.com>
2026-04-24 21:00:04 +0500

    sadasdasd

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,8 +12,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="uz">
-      <body>{children}</body>
+    <html lang="uz" suppressHydrationWarning>
+      <body suppressHydrationWarning>{children}</body>
     </html>
   );
 }
